caa6c318b60c32eabbacd4224c9cfe23c54735ea
Hermann Hubschle <hermann@axiocor.com>
2020-06-23 18:45:48 -0400

    Add fancy tips

--- src/ScottPlot.Demo/PlotTypes/VectorField.cs
+++ src/ScottPlot.Demo/PlotTypes/VectorField.cs
@@ -113,7 +113,38 @@ namespace ScottPlot.Demo.PlotTypes
                     }
                 }
 
-                plt.PlotVectorField(vectors, xs, ys, scaleFactor: 0.3);
+                var field = plt.PlotVectorField(vectors, xs, ys, scaleFactor: 0.25);
+                field.Centered = false;
+            }
+        }
+        public class CustomScaleFactorFancy : PlotDemo, IPlotDemo
+        {
+            public string name { get; } = "Fancy tips";
+            public string description { get; } = "With nice tips";
+
+            public void Render(Plot plt)
+            {
+                double[] xs = DataGen.Range(-1.5, 1.5, .25);
+                double[] ys = DataGen.Range(-1.5, 1.5, .25);
+                Vector2[,] vectors = new Vector2[xs.Length, ys.Length];
+
+                for (int i = 0; i < xs.Length; i++)
+                {
+                    for (int j = 0; j < ys.Length; j++)
+                    {
+                        double x = xs[i];
+                        double y = ys[j];
+                        var e = Math.Exp(-x * x - y * y);
+                        var dx = (1 - 2 * x * x) * e;
+                        var dy = -2 * x * y * e;
+
+                        vectors[i, j] = new Vector2(dx, dy);
+                    }
+                }
+
+                var field = plt.PlotVectorField(vectors, xs, ys, scaleFactor: 0.25);
+                field.Centered = false;
+                field.FancyTip = true;
             }
         }
     }
--- src/ScottPlot/plottables/PlottableVectorField.cs
+++ src/ScottPlot/plottables/PlottableVectorField.cs
@@ -24,6 +24,12 @@ namespace ScottPlot
         private Color[] arrowColors;
         private double scaleFactor;
 
+        public double ArrowheadLength { get; set; } = 0.5;
+        public double ArrowheadWidth { get; set; } = 0.15;
+        public bool Centered { get; set; } = true;
+        public bool FancyTip { get; set; }
+
+
         public PlottableVectorField(Vector2[,] vectors, double[] xs, double[] ys, string label, Color color, Colormap colormap, double scaleFactor)
         {
             //the magnitude squared is faster to compute than the magnitude
@@ -94,6 +100,11 @@ namespace ScottPlot
 
         public override void Render(Settings settings)
         {
+            if (FancyTip)
+                pen.EndCap = LineCap.Flat;
+            else
+                pen.CustomEndCap = new AdjustableArrowCap(2, 2);
+
             for (int i = 0; i < xs.Length; i++)
             {
                 for (int j = 0; j < ys.Length; j++)
@@ -102,17 +113,63 @@ namespace ScottPlot
                     {
                         pen.Color = arrowColors[i * ys.Length + j];
                     }
-                    PlotVector(vectors[i, j], xs[i], ys[j], settings);
+                    if (FancyTip)
+                        PlotVectorFancy(vectors[i, j], xs[i], ys[j], settings);
+                    else
+                        PlotVector(vectors[i, j], xs[i], ys[j], settings);
                 }
             }
         }
 
         private void PlotVector(Vector2 v, double tailX, double tailY, Settings settings)
         {
-            PointF tail = settings.GetPixel(tailX - v.X / 2, tailY - v.Y / 2);
-            PointF end = settings.GetPixel(tailX + v.X / 2, v.Y + tailY - v.Y / 2);
+            PointF tail, end;
+            if (Centered)
+            {
+                tail = settings.GetPixel(tailX - v.X / 2, tailY - v.Y / 2);
+                end = settings.GetPixel(tailX + v.X / 2, tailY + v.Y / 2);
+            }
+            else
+            {
+                tail = settings.GetPixel(tailX, tailY);
+                end = settings.GetPixel(tailX + v.X, tailY + v.Y);
+            }
 
             settings.gfxData.DrawLine(pen, tail, end);
+        }
+        private void PlotVectorFancy(Vector2 v, double tailX, double tailY, Settings settings)
+        {
+            double baseX = tailX - (Centered ? 0 : v.X / 2);
+            double baseY = tailY - (Centered ? 0 : v.Y / 2);
+            double tipX = tailX + (Centered ? v.X / 2 : v.X);
+            double tipY = tailY + (Centered ? v.Y / 2 : v.Y);
+
+            var dx = tipX - baseX;
+            var dy = tipY - baseY;
+            var arrowAngle = Math.Atan2(dy, dx);
+            var headAngle = Math.Atan2(ArrowheadWidth, ArrowheadLength);
+            var sinA1 = Math.Sin(headAngle - arrowAngle);
+            var cosA1 = Math.Cos(headAngle - arrowAngle);
+            var sinA2 = Math.Sin(headAngle + arrowAngle);
+            var cosA2 = Math.Cos(headAngle + arrowAngle);
+            var len = Math.Sqrt(dx * dx + dy * dy);
+            var hypLen = len * Math.Sqrt(ArrowheadLength * ArrowheadLength + ArrowheadWidth * ArrowheadWidth);
+
+            var corner1X = tipX - hypLen * cosA1;
+            var corner1Y = tipY + hypLen * sinA1;
+            var corner2X = tipX - hypLen * cosA2;
+            var corner2Y = tipY - hypLen * sinA2;
+
+            var pts = new PointF[]
+            {
+                settings.GetPixel(baseX, baseY),
+                settings.GetPixel(tipX, tipY),
+                settings.GetPixel(corner1X, corner1Y),
+                settings.GetPixel(tipX, tipY),
+                settings.GetPixel(corner2X, corner2Y),
+            };
+
+            settings.gfxData.DrawLines(pen, pts);
         }
 
         public override string ToString()
