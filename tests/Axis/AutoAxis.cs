cd12d2d8757bb68dd458f37e797525d0a42e4641
Hermann Hubschle <hermann@axiocor.com>
2020-08-04 20:46:36 -0400

    Latest Nuget

--- tests/Axis/AutoAxis.cs
+++ tests/Axis/AutoAxis.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+﻿//using Microsoft.VisualStudio.TestPlatform.Utilities;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
